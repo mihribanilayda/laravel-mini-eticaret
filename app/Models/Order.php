86ae155831a8db031db29c7222a0c9e800611a05
Mihriban İlayda Kurt <mihribanilayda@hotmail.com>
2026-06-19 00:26:52 +0300

    Add Eloquent relationships to models

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use App\Models\User;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    public function user()
+{
+    return $this->belongsTo(User::class);
+}
+
+public function orderItems()
+{
+    return $this->hasMany(OrderItem::class);
+}
     //
 }
--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use App\Models\Order;
+use App\Models\Product;
+
 use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
+    public function order()
+{
+    return $this->belongsTo(Order::class);
+}
+
+public function product()
+{
+    return $this->belongsTo(Product::class);
+}
     //
 }

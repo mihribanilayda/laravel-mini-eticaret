97b3fd90133407171600278a1f7849803f8912ec
Mihriban İlayda Kurt <mihribanilayda@hotmail.com>
2026-06-19 00:30:22 +0300

    Add resource controllers and routes for products, categories, orders

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
+
+Route::resource('categories', CategoryController::class);
+Route::resource('products', ProductController::class);
+Route::resource('orders', OrderController::class);
